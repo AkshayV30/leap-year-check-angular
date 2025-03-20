19e937a2fbd56471a3668106a0bb6b8054d4662e
AkshayV30 <akshayatwork.v30@gmail.com>
2025-03-20 21:21:42 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatRippleModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'leap-year-check-angular';
+
+  year = signal<number | null>(null);
+  result = signal<string>('');
+
+  checkLeapYear(): void {
+    const y = this.year();
+    if (y === null) return;
+
+    if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
+      this.result.set(`${y} is a Leap Year!`);
+    } else {
+      this.result.set(`${y} is NOT a Leap Year.`);
+    }
+  }
 }
